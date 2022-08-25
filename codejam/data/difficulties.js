2f930347f39e51a0cca0359fc7f4ac89bd1f7b1a
Vadim992 <pushtako.vadim867@yandex.ru>
2022-08-25 19:56:47 +0300

    feat: done codejam

---
+++ codejam/data/difficulties.js
@@ -0,0 +1,24 @@
+const difficulties = [
+  {
+    id: 'veryEasy',
+    name: 'Очень легкий'
+  },
+  {
+    id: 'easy',
+    name: 'Легкий'
+  },
+  {
+    id: 'normal',
+    name: 'Средний'
+  },
+  {
+    id: 'hard',
+    name: 'Высокий'
+  },
+  {
+    id: 'veryHard',
+    name: 'Очень высокий'
+  },
+]
+
+export default difficulties
